5e0adbfb82809f3b2e6177a5ee708e0a44f1add4
cljnilsson <ln222pd@student.lnu.se>
2020-05-17 13:42:28 +0200

    Front page previews.

--- mainsite/site.py
+++ mainsite/site.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
-from .models import Section
+from shop.models import Product
 from .core import getContext
 
 def main(request):
@@ -12,5 +12,11 @@ def main(request):
 	}
 
 	context.update(getContext())
+
+	# this is inefficient, make into a single query in the future
+	
+	for i, s in enumerate(context["sections"]):
+		toadd = Product.objects.filter(product_type=s.name)[:4]
+		setattr(context["sections"][i], "products", toadd)
 
 	return HttpResponse(template.render(context, request))
